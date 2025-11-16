df80a10644465cac8134ad497e921e0febf27d5a
Andre Carvalhais <carvalhais@live.com>
2025-11-16 10:22:32 -0300

    func: atualiza .eleventy.js com diretórios estáticos

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
 module.exports = (config) => {
+// diretorios a serem copiados no destino
+config.addPassthroughCopy('./src/css/');
+config.addPassthroughCopy('./src/font/');
+config.addPassthroughCopy('./src/img/');
+config.addPassthroughCopy('./src/js/');
+config.addPassthroughCopy('./src/swiper/');
 	return {
 		dir: {
 			input: 'src',
