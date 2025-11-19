2069b563819bcbfece556b14f8eb217bba9b4a4c
Andre Carvalhais <carvalhais@live.com>
2025-11-19 11:03:07 -0300

    func: inclui arquivo '.htaccess'

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,11 @@
 module.exports = (config) => {
 // diretorios a serem copiados no destino
-config.addPassthroughCopy('./src/css/');
-config.addPassthroughCopy('./src/font/');
-config.addPassthroughCopy('./src/img/');
-config.addPassthroughCopy('./src/js/');
-config.addPassthroughCopy('./src/swiper/');
+config.addPassthroughCopy("src/css");
+config.addPassthroughCopy("src/font");
+config.addPassthroughCopy("src/img");
+config.addPassthroughCopy("src/js");
+config.addPassthroughCopy("src/swiper");
+config.addPassthroughCopy({ "src/root" : "/" });
 	return {
 		dir: {
 			input: 'src',
